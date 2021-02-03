1d724777bc9f7322ede44bbe3b096dfb4caaf9d6
yhaae01 <suryha7fold@gmail.com>
2021-02-03 09:06:13 +0700

    memperbaiki link dashboard di sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -1,15 +1,15 @@
 <div class="main-sidebar">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">SIGDS</a>
+            <a href="<?= base_url('admin') ?>">SIGDS</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">SIGDS</a>
+            <a href="<?= base_url('admin') ?>">SIGDS</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
             <li class="<?= $this->uri->segment(1) == 'admin' ? ' active' : '' ?>">
-                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Data Master</li>
             <li>
@@ -27,8 +27,8 @@
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-envelope"></i><span>Keterangan</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="index-0.html">Pending</a></li>
-                    <li><a class="nav-link" href="index.html">Confirm</a></li>
+                    <li><a class="nav-link" href="#">Pending</a></li>
+                    <li><a class="nav-link" href="#">Confirm</a></li>
                 </ul>
             </li>
             <li class="menu-header">Content</li>
